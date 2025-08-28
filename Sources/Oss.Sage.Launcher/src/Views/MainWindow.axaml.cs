e311df296b9ca5068f9d0094c8146d0d65f9b851
Victor Matia <vmatir@outlook.com>
2025-08-28 21:43:49 +0100

    Added the basic launcher carousel
    Added the basic carousel system that will allow loading mods.
    
    Buttons and options will be added with a more complete system, not now.

--- Sources/Oss.Sage.Launcher/src/Views/MainWindow.axaml.cs
+++ Sources/Oss.Sage.Launcher/src/Views/MainWindow.axaml.cs
@@ -1,8 +1,24 @@
+using System.Diagnostics.CodeAnalysis;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Oss.Sage.Launcher.Views;
 
 public partial class MainWindow : Window
 {
     public MainWindow() => InitializeComponent();
+
+    [SuppressMessage(
+        "csharpsquid",
+        "S2325:Methods and properties that don't access instance data should be static",
+        Justification = "Accesses instance data through Avalonia XAML."
+    )]
+    public void Next(object source, RoutedEventArgs args) => Mods.Next();
+
+    [SuppressMessage(
+        "csharpsquid",
+        "S2325:Methods and properties that don't access instance data should be static",
+        Justification = "Accesses instance data through Avalonia XAML."
+    )]
+    public void Previous(object source, RoutedEventArgs args) => Mods.Previous();
 }
