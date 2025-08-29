a746226b1fade345dc11c0204d3fc802c8f3201f
Victor Matia <vmatir@outlook.com>
2025-08-29 08:45:11 +0100

    Created the importing class
    Now the project is setup to start importing the native library.

---
+++ Sources/WwVegas/Oss.WwVegas.Ww3D2/src/NativeImports/Sdl.cs
@@ -0,0 +1,26 @@
+using System.Runtime.InteropServices;
+
+namespace Oss.WwVegas.Ww3D2.NativeImports;
+
+internal static partial class Sdl
+{
+    private const string LibraryName = "SDL3";
+
+    private static KeyValuePair<string, bool>[] LibraryNames =>
+        [
+            // Add more as necessary. The preferred order is most common to least common desktops
+            new("SDL3.dll", OperatingSystem.IsLinux()),
+            new("libSDL3.so.0.2.16", OperatingSystem.IsLinux()),
+        ];
+
+    static Sdl() =>
+        NativeLibrary.SetDllImportResolver(
+            typeof(Sdl).Assembly,
+            (name, assembly, path) =>
+                NativeLibrary.Load(
+                    LibraryNames.FirstOrDefault(x => x.Value).Key ?? name,
+                    assembly,
+                    path
+                )
+        );
+}
